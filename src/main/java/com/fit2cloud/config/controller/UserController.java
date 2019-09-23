372ca0a094a7677d5e2b4cde21a1b5994aab9a2e
maguohao2018 <guohao.ma@fit2cloud.com>
2019-09-23 18:58:53 +0800

    编写目录

--- src/main/java/com/fit2cloud/config/controller/UserController.java
+++ src/main/java/com/fit2cloud/config/controller/UserController.java
@@ -15,17 +15,17 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("anonymous/impersonateLogin")
 public class UserController {
 
-    @Resource
-    private UserService userService;
-
-    @PostMapping("/getUserkeysByuserId")
-    @ApiOperation("移动端用户查看密钥对")
-    public Object impersonateLogin (@RequestBody User user) throws Exception{
-        try {
-            Object o = userService.getKey(user.getId(), user.getPassword());
-            return o;
-        }catch (Exception e){
-            return e.getMessage();
-        }
-    }
+//    @Resource
+//    private UserService userService;
+//
+//    @PostMapping("/getUserkeysByuserId")
+//    @ApiOperation("移动端用户查看密钥对")
+//    public Object impersonateLogin (@RequestBody User user) throws Exception{
+//        try {
+//            Object o = userService.getKey(user.getId(), user.getPassword());
+//            return o;
+//        }catch (Exception e){
+//            return e.getMessage();
+//        }
+//    }
 }
--- src/main/java/com/fit2cloud/config/service/UserService.java
+++ src/main/java/com/fit2cloud/config/service/UserService.java
@@ -26,76 +26,76 @@ import java.util.*;
 public class UserService {
 
 
-    @Resource
-    private RestTemplate remoteRestTemplate;
-
-    @Resource
-    private ExtGetUserKeyMapper extGetUserKeyMapper;
-
-    //例如 http://103.235.232.207/auth/
-    @Value("${keycloak-server-imperson-address}")
-    private String keyloakServerImpersonAddress;
-
-    //例如 cmp
-    @Value("${keycloak.realm}")
-    private String keycloakRealm;
-
-    private String getToken(String serverAddress, String username, String password) {
-        HttpHeaders tokenHeaders = new HttpHeaders();
-        tokenHeaders.setAccept(new ArrayList<>(Collections.singleton(MediaType.APPLICATION_JSON)));
-        tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, String> mvm = new LinkedMultiValueMap<>();
-        mvm.add("client_id", "cmp-client");
-        mvm.add("username", username);
-        mvm.add("password", password);
-        mvm.add("grant_type", "password");
-        HttpEntity<Object> formEntity = new HttpEntity<>(mvm, tokenHeaders);
-        String tokenUrl = String.format("%s%s", serverAddress, "/realms/" + this.keycloakRealm + "/protocol/openid-connect/token");
-        ResponseEntity<keycloakLoginToken> keycloakTokenEntity = remoteRestTemplate.postForEntity(tokenUrl, formEntity, keycloakLoginToken.class);
-        return Objects.requireNonNull(keycloakTokenEntity.getBody()).getAccessToken();
-    }
-
-    /**
-     * 获取用户accesskey 与secretKey 与sourceId
-     * @param name
-     * @return
-     */
-    public Object getKey(String name, String password) {
-        Map map = this.login(name, password);
-        if (map.get("status").equals(true)) {
-            List<User> userList = extGetUserKeyMapper.getUserList(name);
-            String userId = userList.get(0).getId();
-            return extGetUserKeyMapper.getUserKey(userId).get(0);
-        }
-        return map;
-    }
-
-    /**
-     * 模拟登录认证
-     * @param user
-     * @param password
-     * @return
-     */
-    public Map login(String user, String password) {
-        String msg = "";
-        Map map = new HashMap();
-        if (StringUtils.isBlank(user) || StringUtils.isBlank(password)) {
-            msg = "user or password can't be null";
-            map.put("status", false);
-            map.put("message", msg);
-            return map;
-        }
-        try {
-            //获得token
-            String token = this.getToken(keyloakServerImpersonAddress, user, password);
-            map.put("status", true);
-            return map;
-        }catch (Exception e){
-            msg = e.getMessage() + ": username or password error";
-        }
-        map.put("status", false);
-        map.put("message", msg);
-        return map;
-
-    }
+//    @Resource
+//    private RestTemplate remoteRestTemplate;
+//
+//    @Resource
+//    private ExtGetUserKeyMapper extGetUserKeyMapper;
+//
+//    //例如 http://103.235.232.207/auth/
+//    @Value("${keycloak-server-imperson-address}")
+//    private String keyloakServerImpersonAddress;
+//
+//    //例如 cmp
+//    @Value("${keycloak.realm}")
+//    private String keycloakRealm;
+//
+//    private String getToken(String serverAddress, String username, String password) {
+//        HttpHeaders tokenHeaders = new HttpHeaders();
+//        tokenHeaders.setAccept(new ArrayList<>(Collections.singleton(MediaType.APPLICATION_JSON)));
+//        tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//        MultiValueMap<String, String> mvm = new LinkedMultiValueMap<>();
+//        mvm.add("client_id", "cmp-client");
+//        mvm.add("username", username);
+//        mvm.add("password", password);
+//        mvm.add("grant_type", "password");
+//        HttpEntity<Object> formEntity = new HttpEntity<>(mvm, tokenHeaders);
+//        String tokenUrl = String.format("%s%s", serverAddress, "/realms/" + this.keycloakRealm + "/protocol/openid-connect/token");
+//        ResponseEntity<keycloakLoginToken> keycloakTokenEntity = remoteRestTemplate.postForEntity(tokenUrl, formEntity, keycloakLoginToken.class);
+//        return Objects.requireNonNull(keycloakTokenEntity.getBody()).getAccessToken();
+//    }
+//
+//    /**
+//     * 获取用户accesskey 与secretKey 与sourceId
+//     * @param name
+//     * @return
+//     */
+//    public Object getKey(String name, String password) {
+//        Map map = this.login(name, password);
+//        if (map.get("status").equals(true)) {
+//            List<User> userList = extGetUserKeyMapper.getUserList(name);
+//            String userId = userList.get(0).getId();
+//            return extGetUserKeyMapper.getUserKey(userId).get(0);
+//        }
+//        return map;
+//    }
+//
+//    /**
+//     * 模拟登录认证
+//     * @param user
+//     * @param password
+//     * @return
+//     */
+//    public Map login(String user, String password) {
+//        String msg = "";
+//        Map map = new HashMap();
+//        if (StringUtils.isBlank(user) || StringUtils.isBlank(password)) {
+//            msg = "user or password can't be null";
+//            map.put("status", false);
+//            map.put("message", msg);
+//            return map;
+//        }
+//        try {
+//            //获得token
+//            String token = this.getToken(keyloakServerImpersonAddress, user, password);
+//            map.put("status", true);
+//            return map;
+//        }catch (Exception e){
+//            msg = e.getMessage() + ": username or password error";
+//        }
+//        map.put("status", false);
+//        map.put("message", msg);
+//        return map;
+//
+//    }
 }
