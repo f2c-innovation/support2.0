c05c803ee05b22357ed86f1790e9ff1708cf5415
caixiaoqiu1987 <xiaoqiu.tang@fit2cloud.com>
2019-09-23 11:52:10 +0800

    更改暴露端口8888

--- src/main/java/com/fit2cloud/config/Application.java
+++ src/main/java/com/fit2cloud/config/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = QuartzAutoConfiguration.class)
+@SpringBootApplication(exclude = {QuartzAutoConfiguration.class})
 @EnableDiscoveryClient
 public class Application {
 
